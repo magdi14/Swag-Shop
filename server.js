3b577cdd510d207656b4c2abaaff4e0c3ef701f6
mohamed magdy <[mohamed.magdi@kijamii.com]>
2019-11-19 17:55:44 +0200

    initiation of project

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var routes = require('./routes/routes');
+// var Product = require('./models/product');
+// var WishList = require('./models/wishlist');
+
+var db = mongoose.connect('mongodb://localhost:27017/swag-shop', { useNewUrlParser: true, useUnifiedTopology: true });
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/', routes);
+
+app.listen(3000, function(){
+    console.log("Swag Shop API running on port 3000 ...");
+});
