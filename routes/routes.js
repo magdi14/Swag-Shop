a3ac71ca96f8ab2edcc8d1754d6de70b6c572f7e
magdi14 <mohamed14magdi@gmail.com>
2019-11-27 14:42:31 +0200

    added the whishlist endpoints

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var productCtrl = require('../controllers/productController'); //importing the product controller
-
+var listCtrl = require('../controllers/wishListController');
 var router = express.Router();
 
 //Products
 router.route('/product/:productId').get(productCtrl.getProduct);
-router.route('/product').post(productCtrl.postProduct);
+router.route('/product').post(productCtrl.AddProduct);
 router.route('/products').get(productCtrl.getProducts);
 router.route('/product/:productId').put(productCtrl.editProduct);
 router.route('/product/:productId').delete(productCtrl.deleteProduct);
+
+//Lists
+router.route('/lists').get(listCtrl.getLists);
+router.route('/list/:listId').get(listCtrl.getWishList);
+router.route('/list').post(listCtrl.AddWishList);
+router.route('/list/product').put(listCtrl.AddProductToList);
+router.route('/list/:listId').put(listCtrl.editList);
+router.route('/list/:listId').delete(listCtrl.deleteList);
+
+
+
 
 
 
