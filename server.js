086176761d681ef6b44e6e72aa390898634ca2e8
magdi <mohamed.magdi@kijamii.com>
2019-12-03 16:40:14 +0200

    fixed the CORS problem

--- server.js
+++ server.js
@@ -10,8 +10,14 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 app.use('/', routes);
 
-app.listen(3000, function(){
-    console.log("Swag Shop API running on port 3000 ...");
+app.listen(3008, function(){
+    console.log("Swag Shop API running on port 3008 ...");
 });
